6d8dd4f730195d9f71258b0c15595f70a185cd8c
Manuel Aguilera <manuel@letsgetwonderful.com>
2017-04-20 19:46:51 -0700

    Use contracts instead of facades

--- src/PushBuggyServiceProvider.php
+++ src/PushBuggyServiceProvider.php
@@ -3,17 +3,17 @@
 namespace Wndrfl\PushBuggy;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Contracts\Config\Repository;
+use Illuminate\Contracts\Logging\Log;
 use Monolog\Handler\SlackHandler;
 use Monolog\Logger;
-use Config;
-use Log;
 
 class PushBuggyServiceProvider extends ServiceProvider
 {
-  public function boot() {
-    ‌‌Config::set('services.slackbots', json_decode(env('SLACK_BOTS', '[]')));
+  public function boot(Repository $config, Log $log) {
+    $config->set('services.slackbots', json_decode(env('SLACK_BOTS', '[]')));
 
-    $bots = config('services.slackbots');
+    $bots = $config->get('services.slackbots');
     foreach($bots as $bot) {
         $logger = Log::getMonolog();
         $slackHandler = new SlackHandler($bot->token, $bot->channel, $bot->name, true, null, Logger::INFO);
